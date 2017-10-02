55a3d76896e0ec54700b97a376011de9011fa693
Onesun15 <sunnysharma15@hotmail.com>
2017-10-02 14:13:57 -0400

    buttons fully functional, step through app complete

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 'use strict';
+/* global $*/
 
 // Create your initial store
 const STORE = {
@@ -40,24 +41,53 @@ function nextQuestion () {
   STORE.currentQuestion++;
 }
 
-function generateIntroTextHTML(){
+function generateIntroTextHTML() {
   const introText = `<h1> <h1>Welcome to the Quiz Click below to get started</h1>
         <button class="intro-submit" type="submit">Start Quiz</button></h1>`;
   $('.intro-message').html(introText);
   return introText;
 } 
-function handleStartSubmit(){
+
+function handleStartSubmit() {
   $('.intro-message').on('click', '.intro-submit',(event) => {
     event.preventDefault();
-    console.log(event);
+    // console.log(event);
     STORE.view = 'questions';
     renderContent();
   });
-
 }
 
+function handleFeedbackSubmit() {
+  $('.question-page').on('click', '.feedback-submit',(event) => {
+    event.preventDefault();
+    //console.log(event);
+    STORE.view = 'feedback';
+    renderContent();
+  });
+}
 
-function generateQuestionsHTML(){
+function handleFinalPageView() {
+  $('.feedback').on('click', '.continue-submit',(event) => {
+    event.preventDefault();
+    //console.log(event);
+    STORE.view = 'final';
+    renderContent();
+  });
+}
+
+function handleResetQuiz() {
+  $('.final').on('click', '.play-again-submit',(event) => {
+    event.preventDefault();
+    console.log(event);
+    STORE.view = 'intro';
+    //$('.intro-message').show();
+    //generateIntroTextHTML();
+    renderContent();
+  });
+}
+
+// Make this dynamcially change to each questions content
+function generateQuestionsHTML() {
   const questionsText = `<form id="questionForm">
             <h2>Question</h2>
             <p>
@@ -80,45 +110,51 @@ function generateQuestionsHTML(){
             <input type ="radio" name = "answer" id ="answerChoice5">
             <label for="answerChoice5">Answer</label>
             </p>
-            <button type="submit">Submit</button>
+            <button class="feedback-submit" type="submit">Submit</button>
         </form> `;
-  $('.questionPage').html(questionsText);
+  $('.question-page').html(questionsText);
   return questionsText;
 }
-function generateFeedbackHTML(){
+
+function generateFeedbackHTML() {
   const feedbackText = ` <div class="right">You got it right</div>
         <div class="wrong">You got it wrong</div>
-        <!--current score function-->
-        <button>Continue</button>`;
+        <button class="continue-submit">Continue</button>`;
   $('.feedback').html(feedbackText);
 }
 
-function generateFinalHTML(){
-  const finalText = `  <h1>Thank you for playing</h1>
+function generateFinalHTML() {
+  const finalText = ` <h1>Thank you for playing</h1>
         <div class="score">You scored this out of that</div>
-        <button>Play Again?</button>`;
+        <button class="play-again-submit">Play Again?</button>`;
   $('.final').html(finalText);
 }
 
+// Render content an update STORE.view state
 function renderContent(){
-  if (STORE.view === 'intro'){
+  if (STORE.view === 'intro') {
+    $('.intro-message').show();
+    $('.final').hide();
     generateIntroTextHTML();
   }
   else if(STORE.view === 'questions') {
-    //serve the question and answers html form "questionForm"
+    // serve the question and answers html form "questionForm"
     $('.intro-message').hide();
     generateQuestionsHTML();
   }
-  else if(STORE.view === 'feedback'){
-    //serve the feedback html
+  else if(STORE.view === 'feedback') {
+    // serve the feedback html
+    $('.question-page').hide();
     generateFeedbackHTML();
   }
-
-  else if(STORE.view === 'final'){
-    //serve the feedback html
+  else if(STORE.view === 'final') {
+    // serve the feedback html
+    $('.feedback').hide();
+    $('.final').show();
     generateFinalHTML();
   }
 }
+
 // Constants for permanent DOM identifiers
 const QUESTION_TEXT_EL = $('.question-text');
 const ANSWER_LIST_EL = $('.answers-list');
@@ -147,6 +183,9 @@ function handleAnswerSubmitted() {
 $(() => {
   handleAnswerSubmitted();
   handleStartSubmit();
+  handleFeedbackSubmit();
+  handleFinalPageView();
+  handleResetQuiz();
 });
 
 renderContent();
